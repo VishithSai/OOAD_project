0bcf212a3689b508774bf84e32486903940c8b3f
Sai Vijeta Boddapati <sai.vijeta2021@gmail.com>
2022-03-25 01:03:20 -0700

    added factory dir

---
+++ src/java/factory/factory.java
@@ -0,0 +1,28 @@
+package java;
+
+public class PostFactory {
+  public static Post typepost(Post model) {
+    Post post = null;
+    switch (model) {
+    case Written_post:
+      post = new Written_post_post();
+      break;
+
+    case Image_post:
+      post = new Image_post_post();
+      break;
+
+    case Video_post:
+      post = new Video_post_post();
+      break;
+
+    case GIF_post:
+      post = new GIF_post_post();
+      break;
+
+    default:
+      break;
+    }
+    return post;
+  }
+}
