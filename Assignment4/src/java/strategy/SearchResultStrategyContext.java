2d474b55eb8dc2cb67c4829b30107c5e4320921d
Stephen Jakku <uttamjakku96@gmail.com>
2022-04-25 02:20:18 -0700

    Added Startegy class diagram

--- Assignment4/src/java/strategy/SearchResultStrategyContext.java
+++ Assignment4/src/java/strategy/SearchResultStrategyContext.java
@@ -1,15 +1,12 @@
 public class SearchResultStrategyContext {
     private SearchResultStrategy strategy;
-    //String strategy_name;
 
     public SearchResultStrategyContext(SearchResultStrategy strategy) {
         this.strategy = strategy;
-       // this.strategy_name=strategy_name;
     }
 
     public void setStrategy(SearchResultStrategy strategy) {
         this.strategy = strategy;
-        //this.strategy_name=strategy_name;
     }
 
     public Post[] executeSearch(Post[] posts){
